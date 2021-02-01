9e0bf7e5037ba9525369fd8827eb82887dedc14f
csteinmetz1 <csteinmetz1@gmail.com>
2021-01-31 19:22:21 -0500

    updating to 0.1.6

--- auraloss/utils.py
+++ auraloss/utils.py
@@ -1,6 +1,7 @@
 import torch
 
 def apply_reduction(losses, reduction="none"):
+    """ Apply reduction to collection of losses."""
     if reduction == "mean":
         losses = losses.mean()
     elif reduction == "sum":
