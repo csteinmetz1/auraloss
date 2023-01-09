1d05aeb91f1b5975762d29459083046fab443483
csteinmetz1 <csteinmetz1@gmail.com>
2023-01-09 19:43:24 +0000

    setting simple tests with Actions

--- tests/simple_test.py
+++
@@ -1,9 +0,0 @@
-import torch
-import auraloss
-
-input = torch.rand(8, 2, 44100)
-target = torch.rand(8, 2, 44100)
-
-loss = auraloss.freq.SumAndDifferenceSTFTLoss()
-
-print(loss(input, target))
---
+++ tests/test_auraloss.py
@@ -0,0 +1,89 @@
+import os
+import torch
+import auraloss
+
+
+def test_mrstft():
+    target = torch.rand(8, 2, 44100)
+    pred = torch.rand(8, 2, 44100)
+
+    loss = auraloss.freq.MultiResolutionSTFTLoss()
+    res = loss(pred, target)
+    assert res is not None
+
+
+def test_stft():
+    target = torch.rand(8, 2, 44100)
+    pred = torch.rand(8, 2, 44100)
+
+    loss = auraloss.freq.STFTLoss()
+    res = loss(pred, target)
+    assert res is not None
+
+
+def test_stft_weights_a():
+    target = torch.rand(8, 2, 44100)
+    pred = torch.rand(8, 2, 44100)
+    # test difference weights
+    loss = auraloss.freq.STFTLoss(
+        w_log_mag=1.0,
+        w_lin_mag=0.0,
+        w_sc=1.0,
+        reduction="mean",
+    )
+    res = loss(pred, target)
+    assert res is not None
+
+
+def test_stft_reduction():
+    target = torch.rand(8, 2, 44100)
+    pred = torch.rand(8, 2, 44100)
+    # test the reduction
+    loss = auraloss.freq.STFTLoss(
+        w_log_mag=1.0,
+        w_lin_mag=1.0,
+        w_sc=0.0,
+        reduction="none",
+    )
+    res = loss(pred, target)
+    print(res.shape)
+    assert len(res.shape) > 1
+
+
+def test_sum_and_difference():
+    target = torch.rand(8, 2, 44100)
+    pred = torch.rand(8, 2, 44100)
+    loss = auraloss.freq.SumAndDifferenceSTFTLoss()
+    res = loss(pred, target)
+    assert res is not None
+
+
+def test_melstft():
+    target = torch.rand(8, 2, 44100)
+    pred = torch.rand(8, 2, 44100)
+    # test MelSTFT
+    loss = auraloss.freq.MelSTFTLoss(44100)
+    res = loss(pred, target)
+    assert res is not None
+
+
+def test_melstft_reduction():
+    target = torch.rand(8, 2, 44100)
+    pred = torch.rand(8, 2, 44100)
+    # test MelSTFT with no reduction
+    loss = auraloss.freq.MelSTFTLoss(44100, reduction="none")
+    res = loss(pred, target)
+    assert len(res) > 1
+
+
+def test_multires_mel():
+    target = torch.rand(8, 2, 44100)
+    pred = torch.rand(8, 2, 44100)
+    sample_rate = 44100
+    loss = auraloss.freq.MultiResolutionSTFTLoss(
+        scale="mel",
+        n_bins=64,
+        sample_rate=sample_rate,
+    )
+    res = loss(pred, target)
+    assert res is not None
